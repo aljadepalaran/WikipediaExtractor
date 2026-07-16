e3b8c72bfb97cf870d3a07508f9105859622f2b2
Al Palaran <aljade1902@gmail.com>
2026-07-16 12:08:59 +0100

    comment out wikipedia fetch logic

--- Services/SearchService.cs
+++ Services/SearchService.cs
@@ -9,13 +9,20 @@ public class SearchService(InMemoryDbContext _context) : ISearchService
 {
     public async Task<Response<SearchResult>> RunSearchAsync(string query, int userId)
     {
-        // var result = wikipediaClient.GetAsync("/w/api.php?action=query&list=search&srsearch=ruby&format=json").Result;
-        // Console.WriteLine(result);
+        // Note: This is non-functional due to rate limits. Returning part-static mock.
+        // WikipediaClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+        //     "WikipediaExtractor/1.3 (contact@example.com)"
+        // );
+        // var result = WikipediaClient.GetAsync(
+        //     "/w/api.php?action=query&list=search&srsearch=ruby&format=json").Result;
+
         // Run search against Wikipedia and extract data
         var searchResult = new SearchResult
         {
             SearchTerm = query,
             UserId = userId,
+            TotalResults = 5,
+            SuggestedKeyword = "Wikipedia"
         };
 
         // Save search data into the database
@@ -25,7 +32,7 @@ public class SearchService(InMemoryDbContext _context) : ISearchService
         return Response<SearchResult>.SuccessResponse(searchResult, "", 200);
     }
 
-    private static HttpClient wikipediaClient = new()
+    private static readonly HttpClient WikipediaClient = new()
     {
         // PATH = /w/index.php?title=Special:Search&fulltext=1&search=ruby&ns0=1
         BaseAddress = new Uri("https://en.wikipedia.org")
