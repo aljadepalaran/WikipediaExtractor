46e5a81ef2be10b2d0c23b19637be5af3fdd015d
Al Palaran <aljade1902@gmail.com>
2026-07-16 11:54:09 +0100

    implement existing session validation

--- Services/SessionService.cs
+++ Services/SessionService.cs
@@ -8,35 +8,70 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WikipediaExtractor.Services;
 
-public class SessionService(InMemoryDbContext _context) : ISessionService
+public class SessionService(InMemoryDbContext context) : ISessionService
 {
     public async Task<Response<SessionDto>> LoginAsync(LoginRequest request)
     {
-        var user = await _context.User.FirstOrDefaultAsync(u => u.Username == request.Username);
+        var user = await context.User.FirstOrDefaultAsync(u => u.Username == request.Username);
         if (user != null)
         {
             var passwordHasher = new PasswordHasher<User>();
             var result = passwordHasher.VerifyHashedPassword(user, user.PasswordHash, request.Password);
             if (result == PasswordVerificationResult.Failed)
             {
-                return Response<SessionDto>.FailureResponse(new SessionDto(string.Empty, DateTime.MinValue), "Invalid password", 401);
-            } else
+                Console.WriteLine("Invalid username or password.");
+                return Response<SessionDto>.FailureResponse(new SessionDto(string.Empty, DateTime.MinValue), "Invalid credentials.", 401);
+            }
+            else if (result == PasswordVerificationResult.Success)
             {
-                // TODO: Add validation on existing sessions
-                // TODO: Be more specific with conditions to reach this
-                var session = new Session
+                Console.WriteLine($"Password validation passed. UserId: {user.Id}, UserName: {user.Username}");
+                var existingSession = await context.Session.FirstOrDefaultAsync(session => session.UserId == user.Id);
+                if (existingSession != null)
                 {
-                    UserId = user.Id,
-                    Token = Guid.NewGuid().ToString(),
-                };
-                _context.Session.Add(session);
-                await _context.SaveChangesAsync();
-                return Response<SessionDto>.SuccessResponse(new SessionDto(session.Token, session.ExpiresAt), "Login Successful", 200);
+                    if (existingSession.ExpiresAt < DateTime.UtcNow)
+                    {
+                        Console.WriteLine("Session expired. Creating a new session.");
+                        // Generate a new session if login is successful but the session is expired.
+                        var session = new Session
+                        {
+                            UserId = user.Id,
+                            Token = Guid.NewGuid().ToString(),
+                        };
+                        context.Session.Add(session);
+                        await context.SaveChangesAsync();
+                        return Response<SessionDto>.SuccessResponse(new SessionDto(session.Token, session.ExpiresAt), "Login Successful.", 200);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Session already exists. Returning old session.");
+                        // Return the existing session.
+                        // TODO: Consider deleting the session and re-creating.
+                        return Response<SessionDto>.SuccessResponse(
+                            new SessionDto(existingSession.Token, existingSession.ExpiresAt), "Login Successful.", 200);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("There is no existing session. Creating a new session.");
+                    var session = new Session
+                    {
+                        UserId = user.Id,
+                        Token = Guid.NewGuid().ToString(),
+                    };
+                    context.Session.Add(session);
+                    await context.SaveChangesAsync();
+                    return Response<SessionDto>.SuccessResponse(new SessionDto(session.Token, session.ExpiresAt), "Login Successful.", 200);
+                }
+                
+            }
+            else
+            {
+                return Response<SessionDto>.FailureResponse(new SessionDto(string.Empty, DateTime.MinValue), "Something went wrong.", 500);
             }
         }
         else
         {
-            return Response<SessionDto>.FailureResponse(new SessionDto(string.Empty, DateTime.MinValue), "User not found", 404);
+            return Response<SessionDto>.FailureResponse(new SessionDto(string.Empty, DateTime.MinValue), "Invalid credentials.", 401);
         }
     }
 
@@ -48,7 +83,7 @@ public class SessionService(InMemoryDbContext _context) : ISessionService
 
     public async Task<Response<List<Session>>> GetAllSessionsAsync()
     {
-        var sessions = await _context.Session.ToListAsync();
+        var sessions = await context.Session.ToListAsync();
         return Response<List<Session>>.SuccessResponse(sessions, "", 200);
     }
 }
