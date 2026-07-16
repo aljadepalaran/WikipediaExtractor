136441e794837290be30cf4544dea163240d121f
Al Palaran <aljade1902@gmail.com>
2026-07-16 14:29:24 +0100

    refactor endpoints and add .http

--- Endpoints/RegistrationEndpoints.cs
+++ Endpoints/RegistrationEndpoints.cs
@@ -1,15 +1,22 @@
 using WikipediaExtractor.Contracts;
 using WikipediaExtractor.Interfaces;
+using WikipediaExtractor.Entities;
 
 namespace WikipediaExtractor.Endpoints;
 public static class RegistrationEndpoints
 {
     public static void MapRegistrationEndpoints(this WebApplication app)
     {
-        app.MapPost("/register", async (RegistrationRequest request, IRegistrationService registrationService) =>
-        {
-            var response = await registrationService.RegisterAsync(request);
-            return response;
-        });
+        app.MapPost("/register", HandleRegistrationAsync)
+            .WithName("Register")
+            .WithSummary("Register a user")
+            .WithDescription("Returns a new user object")
+            .Produces<Response<User>>();;
+    }
+
+    private static async Task<Response<User>> HandleRegistrationAsync(RegistrationRequest request, IRegistrationService registrationService)
+    {
+        var response = await registrationService.RegisterAsync(request);
+        return response;
     }
 }
--- Endpoints/SearchEndpoint.cs
+++ Endpoints/SearchEndpoint.cs
@@ -8,17 +8,24 @@ public static class SearchEndpoints
 { 
     public static void MapSearchEndpoints(this WebApplication app)
     {
-        app.MapGet("/search", async (HttpContext context, string query,ISearchService searchService, IAuthService authService) => 
+        app.MapGet("/search", HandleSearchAsync)
+            .WithName("Search")
+            .WithSummary("Search wikipedia")
+            .WithDescription("Returns a search result of wikipedia")
+            .Produces<Response<SearchResult>>();;
+    }
+
+    private static async Task<Response<SearchResult>> HandleSearchAsync(HttpContext context, string query, ISearchService searchService,
+        IAuthService authService)
+    {
+        var authResponse = await authService.AuthenticateRequestAsync(context);
+
+        if (!authResponse.Authenticated)
         {
-            var authResponse = await authService.AuthenticateRequestAsync(context);
+            return Response<SearchResult>.FailureResponse(null, "", 401);
+        }
 
-            if (!authResponse.Authenticated)
-            {
-                return Response<SearchResult>.FailureResponse(null, "", 401);
-            }
-
-            var userId = authResponse.UserId;
-            return await searchService.RunSearchAsync(query, userId);
-        });
+        var userId = authResponse.UserId;
+        return await searchService.RunSearchAsync(query, userId);
     }
 }
